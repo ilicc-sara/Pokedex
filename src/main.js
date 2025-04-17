7ee2aa86376578c33d300a90c6aa322b4885697c
Sara <sarailic431@gmail.com>
2025-04-17 10:04:35 +0200

    fetching pokemons according to types

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
 "use strict";
 import "./style.css";
+
+const typeListEl = document.querySelector(".type-list");
+
+typeListEl.addEventListener("click", function (e) {
+  if (!e.target.closest(".type-item")) return;
+
+  const typeId = e.target.closest(".type-item").dataset.id;
+
+  fetch(`https://pokeapi.co/api/v2/type/${typeId}`)
+    .then((response) => response.json())
+    .then((data) => console.log(data.pokemon[0].pokemon.name))
+    .catch((error) => console.error(error));
+});
